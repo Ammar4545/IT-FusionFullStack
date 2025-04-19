0a5560c2879e44fce00f8bbd95ac1859b6c2524e
AmmarM <ammar.m777.2@gmail.com>
2025-04-19 11:55:38 +0200

    add Employee module

---
+++ IT-Fusion_API/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace IT_Fusion_API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+        public Gender Gender { get; set; }  
+        public string Phone { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
