2b032e9a282c51fcb02050f44c3e9aee7df53677
AmmarM <ammar.m777.2@gmail.com>
2025-04-19 14:06:21 +0200

    getAll api retrive all employees

---
+++ IT-Fusion_API/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using IT_Fusion_API.Models;
+using IT_Fusion_API.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace IT_Fusion_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeFileService _employeeService;
+
+        public EmployeesController(IEmployeeFileService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_employeeService.GetAll());
+
+        
+
+    }
+}
